c796ffe3a8d453c97879ffcd9017634943470560
sridhanalakota <sridhanalakota41@gmail.com>
2025-07-15 21:24:49 +0530

    Add files via upload

---
+++ file1.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Comparison Operators</title>
+  <script src="Operators, Control Flow.js"></script>
+</head>
+<body>
+  <h1>Check console for even/odd result</h1>
+    
+     <h2>Check console for adult/minor result</h2>
+    
+  <h3>Check console for grade</h3>
+   
+<h4>Check console for temperature condition</h4>
+  
+<h5>Open the console to see comparison results</h5>
+</body>
+</html>
